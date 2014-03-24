6f80b46cdda5de5de4d030b16263fae91d2b455b
Legacycz <legacycz@gmail.com>
2014-03-24 13:30:16 +0100

    Ingredients

---
+++ Assets/Scripts/Common/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Viva la Brain Project!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
