507b7802d4cea9d8053eda2be6489b17cd51d3ac
Aleksei Penzentcev <webSwarog@yandex.ru>
2014-03-31 22:51:31 +0200

    Add Kinect Wrapper package and main scene

---
+++ Assets/Scripts/Main/Controlls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseUp()
+    {
+        Debug.Log("onMouse");
+    }
+}
