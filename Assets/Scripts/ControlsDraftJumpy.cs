c193f769f59899cb727cc9a053bcc96910ad9629
Legacycz <legacycz@gmail.com>
2014-04-01 23:55:26 +0200

    Controls concept
    Basic preview of control scheme

---
+++ Assets/Scripts/ControlsDraftJumpy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlsDraftJumpy : MonoBehaviour {
+
+	private Animation animation;
+	private int speed = 5;
+	// Use this for initialization
+	void Start () {
+		animation = this.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("w"))
+			animation.Play();
+	}
+}
---
+++ Assets/Scripts/SelectLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectLevel : MonoBehaviour {
+
+	private Color originalColor;
+	// Use this for initialization
+	void Start () {
+		originalColor = renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void OnMouseOver () {
+		if(Vector3.Distance(GameObject.Find("Player").transform.position,transform.position) < 5)
+			renderer.material.color = Color.green;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = originalColor;
+	}
+}
