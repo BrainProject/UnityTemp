50ca867f86cb96eca0bf1e9a0db49ae77cc16bed
Aleksei Penzentcev <webSwarog@yandex.ru>
2014-04-27 23:00:15 +0200

    Add MouseEvent
    Work together with MouseEmulator

---
+++ Assets/Scripts/GameEvents/Mouse/MovingGameEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Utils.GameEvents;
+using UnityEngine;
+using System.Collections;
+
+public class MovingGameEventArgs : GameEventArgs
+{
+    public MovementIds MovementId { get; set; }
+
+    public MovingGameEventArgs(MovementIds mId)
+    {
+        MovementId = mId;
+    }
+}
