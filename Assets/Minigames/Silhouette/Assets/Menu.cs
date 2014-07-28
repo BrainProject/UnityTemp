112c5821fcdd538ba16b9cdadc1def670c173b1d
Michal Hroš <reaper100@seznam.cz>
2014-07-28 13:52:03 +0200

    Two new minigames: Silhouette - Finding picture and matching silhouette, Similarities - Finding two similar pictures
    To switch between these games, change gameType variable in ResourcePack script before launch.

---
+++ Assets/Minigames/Silhouette/Assets/Menu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    private void StartGame()
+    {
+        //rotate to game view
+        //create main object and attach script
+    }
+
+    /// <summary>
+    /// COROUTINE. Called when selecting item in menu
+    /// </summary>
+    /// <returns></returns>
+    /// <param name="first">First object</param>
+    /// <param name="second">Second object</param>
+    public IEnumerator SwitchView()
+    {
+        /*Mover mover1 = first.GetComponent("Mover") as Mover;
+        Mover mover2 = second.GetComponent("Mover") as Mover;
+        
+        mover2.MoveUp();
+        
+        while (mover1.isMoving || mover2.isMoving)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        mover1.MoveDown();
+        mover2.MoveDown();*/
+        
+        yield return 0;
+    }
+}
