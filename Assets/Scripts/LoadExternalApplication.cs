eb0522def78ddd29d30f73b4b3b727a550062695
Jiri Chmelik <jchmelik@gmail.com>
2014-08-08 19:30:15 +0200

    Update project setting for simpler stand-alone debugging Update splash screen Update Load external application to run "notepad.exe"

--- Assets/Scripts/LoadExternalApplication.cs
+++ Assets/Scripts/LoadExternalApplication.cs
@@ -8,8 +8,9 @@ public class LoadExternalApplication : MonoBehaviour
     {
         if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 15, 100, 30), "Run"))
         {
-            print("This button should run some external content...");
+            print("Running external application");
 
+            System.Diagnostics.Process.Start("notepad.exe");
             //TODO add necessary code here
         }
         
