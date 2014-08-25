bc21c7641a82d5ed68ef1d8eddab0aa2c4cfeb59
Tonda H <tonislavovic@seznam.cz>
2014-08-25 15:33:22 +0200

    added point avatar

---
+++ Assets/Minigames/Tonda Project/PointAvatar/ExtendsAvatar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kinect;
+
+public class ExtendsAvatar : AvatarController {
+
+	// Use this for initialization
+	/*void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+}
