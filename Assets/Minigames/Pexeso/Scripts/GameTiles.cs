d7cfa6fa54b30f4e36c7ef522dd715bb7b654a80
Jiri Chmelik <jchmelik@gmail.com>
2014-09-01 20:54:33 +0200

    Update Pexeso (and its variants) mini-games

--- Assets/Minigames/Pexeso/Scripts/GameTiles.cs
+++ Assets/Minigames/Pexeso/Scripts/GameTiles.cs
@@ -7,13 +7,13 @@ public class GameTiles : MonoBehaviour
     public static GameObject[] createTiles(int rows, int columns, GameObject tilePrefab, string tileTag)
     {
         GameObject[] gameTiles = new GameObject[rows * columns];
-        for (int i = 0; i < columns; i++)
+        for (int i = 0; i < rows; i++)
         {
-            for (int o = 0; o < rows; o++)
+            for (int o = 0; o < columns; o++)
             {
-                gameTiles[rows * i + o] = Instantiate(tilePrefab) as GameObject;
-                gameTiles[rows * i + o].transform.position = new Vector3((i * 1.2f) - (0.1125f * (columns * columns)), (o * 1.2f) - (0.05625f * (rows * rows)), -1);//flipAnimation them
-                gameTiles[rows * i + o].tag = tileTag;
+				gameTiles[columns * i + o] = Instantiate(tilePrefab) as GameObject;
+				gameTiles[columns * i + o].transform.position = new Vector3((o * 1.2f) - (0.1125f * (columns * columns)),  (0.225f * (rows * rows)) - (i * 1.2f), -1);//flipAnimation them
+				gameTiles[columns * i + o].tag = tileTag;
             }
         }
 
