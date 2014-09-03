39188a023139fe77239a3a375fb99f36fcdca15d
Tonda H <tonislavovic@seznam.cz>
2014-09-03 21:41:41 +0200

    Rename Tonda Project to SocialGame, remove pointman from scene and add pointavatars.

---
+++ Assets/Minigames/SocialGame/GameObjectEx.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectEx{
+
+	public static GameObject findGameObjectWithNameTag(string name, string tag)
+	{
+		GameObject[] objects =GameObject.FindGameObjectsWithTag(tag);
+		foreach(GameObject obj in objects)
+		{
+			if(obj.name.Equals(name))
+			{
+				return obj;
+			}
+		}
+		return null;
+	}
+}
---
+++ Assets/Minigames/SocialGame/PointAvatar/GestGame/CheckCol.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckCol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnColisionEnter(Collision col)
+	{
+		Debug.Log("colision");
+	}
+}
