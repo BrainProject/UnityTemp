22c6e7bccdff251605405082bb51ff2d0da6eb09
Tonda H <tonislavovic@seznam.cz>
2014-10-13 23:02:24 +0200

    add autism   more friendly counter, lines for ballons

--- Assets/Minigames/SocialGame/Scripts/FitCounter.cs
+++ Assets/Minigames/SocialGame/Scripts/FitCounter.cs
@@ -6,6 +6,12 @@ namespace SocialGame{
 		public  int max;
 		private int count;
 
+		public float space;
+		public GameObject line;
+		public SpriteRenderer[] lines;
+		public Vector3 startGUI;
+
+
 		public int selectedMovements;
 
 		public GameObject[] movements ;
@@ -13,26 +19,62 @@ namespace SocialGame{
 		void Start () {
 			selectedMovements = Random.Range(0,movements.Length);
 			GameObject.Instantiate(movements[selectedMovements]);
+			drawCount();
+			redraw();
 		}
 
 
 		public void  nextComplete()
 		{
 			count ++;
-			if(count > max)
+			if(count >= max)
 			{
+				redraw();
 				LevelManager.finish();
 			}
 			else
 			{
 				GameObject.Instantiate(movements[selectedMovements]);
+				redraw();
 			}
 		}
 
-		//bugbug
-		void OnGUI()
+		void drawCount()
 		{
-			GUI.TextArea(new Rect(0,0,60,30),count.ToString());
+			lines = new SpriteRenderer[max];
+			for(int i = 0; i < max; i++)
+			{
+				if(line)
+				{
+					Vector3 position = startGUI + Vector3.right * space * i;
+					GameObject clone = (GameObject) GameObject.Instantiate(line,position, Quaternion.identity);
+					clone.transform.parent = transform;
+					SpriteRenderer cloneRender = clone.GetComponent<SpriteRenderer>();
+					if(cloneRender)
+						lines[i] = cloneRender;
+					else
+					{
+						Debug.LogWarning("On"+ gameObject.name +"atribut line must be sprite");
+						lines = null;
+						break;
+					}
+				}
+			}
+		}
+
+		void redraw()
+		{
+			for(int i = 0; i < lines.Length;i++)
+			{
+			   if(i < max - count)
+				{
+					lines[i].enabled = true;
+				}
+				else
+				{
+					lines[i].enabled = false;
+				}
+			}
 		}
 	}
 }
