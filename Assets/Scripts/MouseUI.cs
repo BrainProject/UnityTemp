c52cb670772628719a269f54cd96101bf3126ca5
Legacycz <snake2112@seznam.cz>
2015-03-23 13:08:40 +0100

    Add basic UI driven cursor.

---
+++ Assets/Scripts/MouseUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MouseUI : MonoBehaviour {
+	public Sprite normal;
+	public Sprite selected;
+	public Image image;
+
+	// Use this for initialization
+	void Start () {
+		image.sprite = normal;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Input.mousePosition;
+	}
+
+	public void MouseDown()
+	{
+		image.sprite = selected;
+	}
+
+	public void MouseUp()
+	{
+		image.sprite = normal;
+	}
+}
