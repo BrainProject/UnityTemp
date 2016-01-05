8b6fee3874f36b748fa516105ffe80712ce0bdcb
hci <hci@Dual.ad.fi.muni.cz>
2016-01-05 19:28:04 +0100

    Fix help play loop. Change location of Kinect DLLs.

--- Assets/Scripts/HelpEnd.cs
+++ Assets/Scripts/HelpEnd.cs
@@ -2,21 +2,29 @@
 using System.Collections;
 
 public class HelpEnd : StateMachineBehaviour {
+    private bool isGUIVisible = false;
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        isGUIVisible = false;
+	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	    if (stateInfo.normalizedTime >= 0.999f)
+        {
+            if(!isGUIVisible)
+            {
+                MGC.Instance.neuronHelp.GetComponent<Game.NEWBrainHelp>().helpObject.ShowButtons();
+                isGUIVisible = true;
+            }
+        }
+	}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        MGC.Instance.neuronHelp.GetComponent<Game.NEWBrainHelp>().helpObject.ShowButtons();
-	}
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+	//}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
