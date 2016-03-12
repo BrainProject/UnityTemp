b5897b2f250f743fd4b9dea1f532e3de91f42b7d
Michal Hroš <reaper100@seznam.cz>
2016-03-12 18:18:18 +0100

    Added Maze minigame. - Added to MinigamesConfiguration, including images for minigame and for difficulties (just quick mock ups) - In Minigame code, added call to obtain difficulty from MGC and calls when hame is won or when game is lost (WinMinigame, LoseMinigame) - note: github tutorial mentions only method called 'FinishMinigame', which actually does not exist - There is no info on how to add a minigame to any Newron game menu, so Maze cannot be launched from Newron menu right now

---
+++ Assets/Minigames/Maze/Scripts/PlayerScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MinigameMaze
+{
+	/// <summary>
+	/// Player script. Computes player movement according to mouse position.
+	/// </summary>
+	public class PlayerScript : MonoBehaviour
+	{
+        /// <summary>
+        /// Should the holder for player object be used?
+        /// </summary>
+        public bool useHolder;
+        
+        /// <summary>
+		/// Used for mouse pointer detection.
+		/// </summary>
+		private Ray ray;
+		/// <summary>
+		/// Used for mouse pointer detection. Object under mouse cursor.
+		/// </summary>
+		private RaycastHit hit;
+
+		/// <summary>
+		/// True if user currently moves player object.
+		/// </summary>
+		private bool isPlayerTapped;
+
+		// Use this for initialization
+		void Start ()
+		{
+            if (!useHolder)
+            {
+                GameObject.Find("Holder").SetActive(false);
+            }
+		}
+
+        public Vector3 difference;
+
+		// Update is called once per frame
+		void Update ()
+		{
+			if (Input.GetMouseButton (0))
+			{
+                if (isPlayerTapped)
+                {
+                    Vector3 mousePos = Input.mousePosition;
+                    mousePos.z = 10f + 1.95f;
+                    Vector3 mouseCoordinates = Camera.main.ScreenToWorldPoint(mousePos);
+                    transform.position = mouseCoordinates - difference;
+                    return;
+                }
+                
+                ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				Vector3 position = new Vector3 (0, 1, -10);
+				if (Physics.Raycast (position, ray.direction, out hit))
+				{
+                    if (hit.collider.name == "PlayerObject" || hit.collider.name == "Holder")
+					{
+						Vector3 mousePos = Input.mousePosition;
+						mousePos.z = 10f + 1.95f;
+						Vector3 mouseCoordinates = Camera.main.ScreenToWorldPoint (mousePos);
+                        difference = mouseCoordinates - transform.position;
+                        transform.position = mouseCoordinates - difference;
+						isPlayerTapped = true;
+					}
+					
+				}
+			}
+			else
+			{
+				if(isPlayerTapped)
+				{
+					isPlayerTapped = false;
+				}
+			}
+		}
+	}
+}
