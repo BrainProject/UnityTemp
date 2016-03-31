a5d036f99b168be7f7b50368a270da9840b250df
unknown <xhanuso1@Lynkeus.ad.fi.muni.cz>
2016-03-31 23:37:40 +0200

    Added hand colliders to avatar and two collision detectors (back button and help) There's an error in GoBackAction method.

---
+++ Assets/Scripts/GSIv2/CollisionLoading.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using Image = UnityEngine.UI.Image;
+
+//abstract class for colliders
+abstract public class CollisionLoading : MonoBehaviour {
+
+
+    private bool activated;         //collision has been detected
+    private bool done;              //image is whole drawn
+    protected Image img;
+
+    protected virtual void Start ()
+    {
+        if (GetComponent<Image>())
+        {
+            img = GetComponent<Image>();
+            img.fillAmount = 0;
+            if(!img.sprite)
+            {
+                this.enabled = false;
+            }
+        }
+        else
+        {
+            this.enabled = false;
+        }
+        activated = false;
+        done = false;
+    }
+	
+	void Update ()
+    {
+
+        if (activated)
+        {
+            if (img.fillAmount < 1)
+            {
+                img.fillAmount += (0.6f * Time.deltaTime);
+            }
+            else
+            {
+                done = true;
+            }
+        }
+        else
+        {
+            img.fillAmount = 0;
+        }
+
+        if (done)
+        {
+            Action();
+            done = false;
+            img.fillAmount = 0;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        activated = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        activated = false;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract void Action();
+    
+}
---
+++ Assets/Scripts/GSIv2/HandColiderMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace GSIv2
+{
+    public class HandColiderMovement : MonoBehaviour
+    {
+        public Transform hand;
+
+        void Start()
+        {
+           
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            
+
+            //GetComponent<Rigidbody2D>().velocity = (movement * 800.0f * Time.deltaTime);
+            transform.Translate(movement);           
+            transform.position = Camera.main.WorldToScreenPoint(hand.position);
+            
+        }
+    }
+
+}
