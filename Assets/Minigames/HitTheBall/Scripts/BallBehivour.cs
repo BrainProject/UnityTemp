f633e483883dc54198724f9334581bc21cc53569
malgi <marketa.marki@centrum.cz>
2016-11-07 14:29:38 +0100

    HitTheBall game created (from GSIv2)

---
+++ Assets/Minigames/HitTheBall/Scripts/BallBehivour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum BallStates { Start, Game}
+
+public class BallBehivour : MonoBehaviour {
+    private BallStates state;
+    private float countdown = 10f;
+
+	// Use this for initialization
+	void Start () {
+        state = BallStates.Start;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch(state)
+        {
+            case BallStates.Start:
+                if (countdown <= 0)
+                {
+                    GetComponent<Rigidbody2D>().isKinematic = false;
+                    state = BallStates.Game;
+                }
+                else
+                {
+                    countdown -= Time.deltaTime;
+                }
+                break;
+            case BallStates.Game:
+
+                break;
+        }
+	    
+	}
+}
