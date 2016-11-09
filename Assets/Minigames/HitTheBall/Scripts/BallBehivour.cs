9418de893ecabab3937c99d90a15cc5d40de1855
malgi <marketa.marki@centrum.cz>
2016-11-09 22:55:03 +0100

    Mechanism of switching the players and counting the score (HitTheBall game)

--- Assets/Minigames/HitTheBall/Scripts/BallBehivour.cs
+++ Assets/Minigames/HitTheBall/Scripts/BallBehivour.cs
@@ -1,40 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace HitTheBall
 {
-    enum BallStates { Start, Game}
+    
 
     public class BallBehivour : MonoBehaviour
     {
-
-        private BallStates state;
+        public LevelManagerBall levelManager;
+        
         private float countdown = 10f;
 
         // Use this for initialization
         void Start()
         {
-            state = BallStates.Start;
+            levelManager.state = GameStates.Start;
             GetComponent<Rigidbody2D>().isKinematic = true;
+            
         }
 
         // Update is called once per frame
         void Update()
         {
-            switch (state)
+            switch (levelManager.state)
             {
-                case BallStates.Start:
+                case GameStates.Start:
+                    
                     if (countdown <= 0)
                     {
                         GetComponent<Rigidbody2D>().isKinematic = false;
-                        state = BallStates.Game;
+                        levelManager.state = GameStates.Game;
                     }
                     else
                     {
                         countdown -= Time.deltaTime;
                     }
                     break;
-                case BallStates.Game:
+                case GameStates.Game:
 
                     break;
             }
@@ -43,17 +46,72 @@ namespace HitTheBall
 
         void OnCollisionEnter2D(Collision2D col)
         {
-            if (col.gameObject.name == "2Dcollider")
+
+            switch (levelManager.state)
             {
+                case GameStates.Start:
 
-                // angle between the collision point and the ball
-                Vector2 direction = col.contacts[0].point - new Vector2(transform.position.x, transform.position.y);
-                // opposite (-Vector3) and normalize it
-                direction = -direction.normalized;
+                    if (countdown <= 0)
+                    {
+                        GetComponent<Rigidbody2D>().isKinematic = false;
+                        levelManager.state = GameStates.Game;
+                    }
+                    else
+                    {
+                        countdown -= Time.deltaTime;
+                    }
+                    break;
+                case GameStates.Game:
+                    // 2Dcolliders are on the hands of active player
+                    if (col.gameObject.name == "2Dcollider")
+                    {
+                        hitBall(col);
+                        levelManager.score++;
+                        levelManager.scoreText.text = string.Format(levelManager.score.ToString());
+                        if (col.gameObject.tag == "Player1")
+                        {
+                            
+                            levelManager.player1turn = false;
+                        }
+                        else
+                        {
+                            levelManager.player1turn = true;
+                        }
 
-                // combination of vector.up and the direction of the hit
-                gameObject.GetComponent<Rigidbody2D>().AddForce((new Vector2(Vector2.up.x * direction.x, Vector2.up.y * direction.y) * 800), ForceMode2D.Force);
+                    }
+                    break;
             }
+
+
+
+            /*if (state == BallStates.Start)
+            {
+                if (col.transform.root.tag == "Player1")
+                {
+                    levelManager.player1turn = true;
+                }
+                else
+                {
+                    levelManager.player1turn = false;
+                }
+                Debug.Log("Player1 is playing? " + levelManager.player1turn);
+                GetComponent<Rigidbody2D>().isKinematic = false;
+                state = BallStates.Game;
+            }*/
+
+            
+        }
+
+        // give movement to the hit ball
+        private void hitBall(Collision2D col)
+        {
+            // angle between the collision point and the ball
+            Vector2 direction = col.contacts[0].point - new Vector2(transform.position.x, transform.position.y);
+            // opposite (-Vector3) and normalize it
+            direction = -direction.normalized;
+
+            // combination of vector.up and the direction of the hit
+            gameObject.GetComponent<Rigidbody2D>().AddForce((new Vector2(Vector2.up.x * direction.x, Vector2.up.y * direction.y) * 800), ForceMode2D.Force);
         }
     }
 }
---
+++ Assets/Minigames/HitTheBall/Scripts/LevelManagerBall.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum GameStates { Start, Game, GameOver, Win }
+
+public class LevelManagerBall : MonoBehaviour {
+
+    // which player is active
+    public bool player1turn;
+
+    public int WinHits; // number of hits needed for the win
+
+    public Text player1text;
+    public Text player2text;
+    public Text scoreText;
+
+    public GameObject Player1;
+    public GameObject Player2;
+
+    public GameStates state;
+
+    public GameObject ball;
+
+    public int score;
+
+    void Start () {
+        score = 0;
+        chooseLevel();
+        player1text.enabled = true;
+        player2text.enabled = false;
+        ActivePlayer(Player1, Player2);
+        
+    }
+	
+	void Update () {
+        CircleCollider2D[] handColliders;
+
+        switch (state)
+        {
+            case GameStates.Game:
+                if (player1turn)
+                {
+                    player1text.enabled = true;
+                    player2text.enabled = false;
+
+                    handColliders = Player1.GetComponentsInChildren<CircleCollider2D>();
+                    foreach (CircleCollider2D collider in handColliders)
+                    {
+                        collider.enabled = true;
+                    }
+
+                    handColliders = Player2.GetComponentsInChildren<CircleCollider2D>();
+                    foreach (CircleCollider2D collider in handColliders)
+                    {
+                        collider.enabled = false;
+                    }
+
+                    ActivePlayer(Player1, Player2);
+                }
+                else
+                {
+                    player1text.enabled = false;
+                    player2text.enabled = true;
+
+                    handColliders = Player1.GetComponentsInChildren<CircleCollider2D>();
+                    foreach (CircleCollider2D collider in handColliders)
+                    {
+                        collider.enabled = false;
+                    }
+
+                    handColliders = Player2.GetComponentsInChildren<CircleCollider2D>();
+                    foreach (CircleCollider2D collider in handColliders)
+                    {
+                        collider.enabled = true;
+                    }
+
+                    ActivePlayer(Player2, Player1);
+                }
+
+                // win : get the number of needed hits
+                if (score >= WinHits)
+                {
+                    Win();
+                    state = GameStates.Win;
+                }
+                if (!isBallInGame(ball))
+                {
+                    
+                    GameOver();
+                    state = GameStates.GameOver;
+                }
+                break;
+            case GameStates.GameOver:
+
+                break;
+            case GameStates.Win:
+
+                break;
+        }
+
+        // bude se ti hodit, nezapomeň smazat
+        // loading number of pieces according to the set difficulty
+        /*switch (MGC.Instance.selectedMiniGameDiff)
+        {
+            case 0:
+                numberPieces = 20;
+                break;
+            case 1:
+                numberPieces = 44;
+                break;
+            default:
+                numberPieces = PIECES_RECOMMENDED;
+                break;
+        }*/
+
+
+    }
+
+    private bool isBallInGame(GameObject ball)
+    {
+        return (ball.GetComponent<Transform>().position.y >= 0);
+    }
+
+    void GameOver()
+    {
+        MGC.Instance.LoseMinigame();
+        Debug.Log("Game is over!");
+    }
+
+    void Win()
+    {
+        MGC.Instance.WinMinigame();
+        Debug.Log("You win!");
+    }
+
+    private void chooseLevel()
+    {
+        Debug.Log("Difficulty: " + MGC.Instance.selectedMiniGameDiff);
+        switch (MGC.Instance.selectedMiniGameDiff)
+        {
+            case 0:
+                WinHits = 10;
+                break;
+            case 1:
+                WinHits = 15;
+                break;
+            case 2:
+                WinHits = 5000;
+                break;
+            default:
+                WinHits = 10;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Makes deactive player transparent and active visible.
+    /// </summary>
+    /// <param name="activePlayer">visible player</param>
+    /// <param name="deactivePlayer">transparent player</param>
+    private void ActivePlayer(GameObject activePlayer, GameObject deactivePlayer)
+    {
+        foreach (SkinnedMeshRenderer renderer in deactivePlayer.GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            renderer.material.shader = Shader.Find("Transparent/Diffuse");
+            renderer.material.color = new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, 0.4f);
+        }
+        foreach (SkinnedMeshRenderer renderer in activePlayer.GetComponentsInChildren<SkinnedMeshRenderer>())
+        {
+            renderer.material.shader = Shader.Find("Standard");
+        }
+    }
+}
